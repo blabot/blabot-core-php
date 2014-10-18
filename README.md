23172fa6871d52a73dce2530196e7f48195789aa
Tomas Kuba <tomas.kuba.cz@gmail.com>
2014-10-18 13:25:39 +0200

    Project setup

---
+++ README.md
@@ -0,0 +1,4 @@
+Blabot generator
+================
+
+Exceptionally language faithful generator of dummy text especially (but not exclusively) designed for Czech typography.
---
+++ tests/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace TomasKuba\Blabot\Tests;
+
+
+class Test extends \PHPUnit_Framework_TestCase {
+
+    public function test_Is_PHPUnit_working()
+    {
+        $this->assertTrue(true);
+    }
+}
+ 
