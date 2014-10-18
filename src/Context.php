84557f1fb0edef5b7aebc5530d88f22f0de12974
Tomas Kuba <tomas.kuba.cz@gmail.com>
2014-10-18 17:25:24 +0200

    Context

--- src/Context.php
+++ src/Context.php
@@ -4,6 +4,21 @@
 namespace TomasKuba\Blabot;
 
 
-class Context {
+class Context
+{
+    private $services = array();
 
+    public function getService($name)
+    {
+        if (!array_key_exists($name,$this->services)){
+            throw new \RuntimeException("Unknown service: '$name'");
+        } else {
+            return $this->services[$name];
+        }
+    }
+
+    public function addService($name, $value)
+    {
+        $this->services[$name] = $value;
+    }
 } 
--- tests/ContextTest.php
+++ tests/ContextTest.php
@@ -4,7 +4,39 @@
 namespace TomasKuba\Blabot\Tests;
 
 
+use TomasKuba\Blabot\Context;
+
 class ContextTest extends \PHPUnit_Framework_TestCase {
 
+    /**
+     * @test
+     * @expectedException \RuntimeException
+     */
+    public function givenNoServiceThrows()
+    {
+        $c = new Context();
+        $c->getService('service');
+    }
+
+    /**
+     * @test
+     * @expectedException \RuntimeException
+     */
+    public function givenServiceButRequestedAnotherThrows()
+    {
+        $c = new Context();
+        $c->addService('service1', null);
+        $c->getService('service2');
+    }
+
+    /**
+     * @test
+     */
+    public function givenServiceReturnsServiceByName()
+    {
+        $c = new Context();
+        $c->addService("service name", "service value");
+        $this->assertEquals("service value", $c->getService("service name"));
+    }
 }
  
