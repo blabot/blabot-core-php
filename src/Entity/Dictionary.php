595ca8d1c5b936714e5b03ad2d8b27b348f3e690
Tomas Kuba <tomas.kuba.cz@gmail.com>
2014-10-19 17:21:18 +0200

    Basic Dictionary

--- src/Entity/Dictionary.php
+++ src/Entity/Dictionary.php
@@ -1,9 +1,83 @@
 <?php
 
 
-namespace TomasKuba\Blabot\src\Entity;
+namespace TomasKuba\Blabot\Entity;
 
 
 class Dictionary {
 
+    /** @var array */
+    private $words = array();
+
+    /** @var array */
+    private $sentences = array();
+
+    /**
+     * return bool
+     */
+    public function hasWords()
+    {
+        return !empty($this->words);
+    }
+
+    /**
+     * return bool
+     */
+    public function hasSentences()
+    {
+        return !empty($this->sentences);
+    }
+
+    public function addWord($word)
+    {
+        if (!$this->hasWord($word)) {
+            $this->words[mb_strlen($word)][] = $word;
+        }
+    }
+
+    public function hasWord($word)
+    {
+        $length = mb_strlen($word);
+        return ($this->hasWordGroupOfLength($length) && in_array($word, $this->words[$length]));
+    }
+
+    public function addSentence($sentence)
+    {
+        if (!in_array($sentence, $this->sentences)){
+            $this->sentences[] = $sentence;
+        }
+    }
+
+    public function getWordOfLength($length)
+    {
+        if (!$this->hasWordGroupOfLength($length)) {
+            return "";
+        }
+
+        return $this->words[$length][array_rand($this->words[$length])];
+    }
+
+    public function countWordsOfLength($length)
+    {
+        return count($this->words[$length]);
+    }
+
+    /**
+     * @param $length
+     * @return bool
+     */
+    public function hasWordGroupOfLength($length)
+    {
+        return array_key_exists($length, $this->words);
+    }
+
+    public function countSentences()
+    {
+        return count($this->sentences);
+    }
+
+    public function getSentence()
+    {
+        return $this->sentences[array_rand($this->sentences)];
+    }
 }
