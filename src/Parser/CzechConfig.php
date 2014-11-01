dcaed20adf55daf97aacf1b79fcd30d74bfcff3c
Tomas Kuba <tomas.kuba.cz@gmail.com>
2014-11-01 12:17:20 +0100

    Improved Czech lang. config

--- src/Parser/CzechConfig.php
+++ src/Parser/CzechConfig.php
@@ -10,7 +10,17 @@ class CzechConfig extends LanguageConfig
     function __construct()
     {
         $this->language = LanguageConfig::LANGUAGE_CZECH;
-        $this->sentenceDelimiters = array(".","?","!");
-
+        $this->normalizingRules = array(
+            array("/[\n\r]/u"," "),     // remove new lines
+            array('/[°′″„“‚‘"]/u', ''), // strip strange/complicated chars
+            array("/\.\.\./u","."),     // change tree dots i one dot
+            array("\t", ' '),           // replace tab by space
+            array("/ , /u",", "),       // correct spaces before comas
+            array("/\s+/u"," "),        // replace one or more white char by space
+            array("/^\s+|\s$/u",""),    //remove leading/trailing spaces
+        );
+        $this->badWords = array("kur","píč", "čurá", "mrd", "srá");
+        $this->sentenceDelimiters = array(".","?","!", "…");
+        $this->specialWordChars = array("'", "—", ".");
     }
 }
